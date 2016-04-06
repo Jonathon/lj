0b835b64ccd417c03cf718bb18eb970019d8c09c
Jon <johndodd547@gmail.com>
2016-04-06 20:18:59 +0930

    ES LINT
    modified rules in esLint  file to suit project.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 const gulp = require('gulp');
 //gulp dependencies go here
 const babel = require('gulp-babel')
+const eslint = require('gulp-eslint');
+
+
 gulp.task('default', function() {
     //Gulp tasks go here
+    //Run ES lint
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+	.pipe(eslint())
+	.pipe(eslint.format());
+	//Node Source
     gulp.src("es6/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest("dist"));
